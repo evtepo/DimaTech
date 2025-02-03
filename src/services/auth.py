929e27b510b8739c87d46fbae4fd98f1bc1c986f
evtepo <andrey_laguta00@mail.ru>
2025-02-03 23:39:34 +1000

    add auth logic

---
+++ src/services/auth.py
@@ -0,0 +1,68 @@
+from async_fastapi_jwt_auth.auth_jwt import AuthJWT
+from fastapi.responses import JSONResponse
+from passlib.context import CryptContext
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
+
+from configs.settings import settings
+from models.user import User
+from repository.repository import BaseRepository
+from schemas.user import UserCreate, UserLogIn
+from utils.error_handling import error_response
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+async def check_current_user(session: AsyncSession, authorize: AuthJWT):
+    try:
+        await authorize.jwt_required()
+        current_user = await authorize.get_jwt_subject()
+        return current_user
+    except Exception:
+        return JSONResponse({"msg": "Invalid token."}, status_code=status.HTTP_401_UNAUTHORIZED)
+
+
+async def create_user_profile(
+    user: UserCreate,
+    session: AsyncSession,
+    repository: BaseRepository,
+):
+    data = user.model_dump()
+    data["password"] = get_password_hash(data["password"])
+
+    new_user = await repository.create(data, User, session)
+    if not new_user:
+        return error_response("User with this email already exists", status.HTTP_400_BAD_REQUEST)
+
+    return new_user
+
+
+async def log_in_to_account(
+    user: UserLogIn,
+    session: AsyncSession,
+    authorize: AuthJWT,
+    repository: BaseRepository,
+):
+    data = user.model_dump()
+    del data["password"]
+    instance = await repository.get_single(User, session, **data)
+    if not instance or not verify_password(user.password, instance.password):
+        return error_response("Invalid username or password", status.HTTP_401_UNAUTHORIZED)
+
+    access_token = await authorize.create_access_token(
+        subject=instance.email,
+        expires_time=settings.access_token_expire_in_seconds,
+    )
+
+    await authorize.set_access_cookies(access_token)
+
+    return {"access_token": access_token}
