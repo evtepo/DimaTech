3b99a8d553e1a67346ff6cee61c9b720ce05668c
evtepo <andrey_laguta00@mail.ru>
2025-02-03 23:40:11 +1000

    add token settings class

---
+++ src/schemas/auth.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+from configs.settings import settings
+
+
+class TokenSettings(BaseModel):
+    authjwt_secret_key: str = settings.token_secret_key
+    authjwt_algorithm: str = "HS256"
+    authjwt_token_location: set = {"cookies"}
