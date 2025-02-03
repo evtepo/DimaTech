cd65da2dd599869e1aaab283a20236b2d6826bce
evtepo <andrey_laguta00@mail.ru>
2025-02-03 23:43:35 +1000

    add auth router

---
+++ src/api/v1/auth.py
@@ -0,0 +1,41 @@
+from async_fastapi_jwt_auth.auth_jwt import AuthJWT
+from fastapi import APIRouter, Depends
+from starlette import status
+
+from schemas.auth import TokenSettings
+from schemas.user import UserCreate, UserInfo, UserLogIn
+from services.auth import create_user_profile, log_in_to_account
+from utils.dependency import db_dependency, repository_dependency
+
+
+router = APIRouter(prefix="/api/v1/auth", tags=["Auth"])
+
+
+@AuthJWT.load_config
+def get_config():
+    return TokenSettings()
+
+
+@router.post(
+    "/register",
+    summary="Sign up a new user.",
+    status_code=status.HTTP_201_CREATED,
+    response_model=UserInfo | None,
+)
+async def create_user(user: UserCreate, session: db_dependency, repository: repository_dependency):
+    return await create_user_profile(user, session, repository)
+
+
+@router.post(
+    "/login",
+    summary="Log in to account.",
+    status_code=status.HTTP_200_OK,
+    response_model=dict[str, str],
+)
+async def login(
+    user: UserLogIn,
+    session: db_dependency,
+    repository: repository_dependency,
+    authorize: AuthJWT = Depends(),
+):
+    return await log_in_to_account(user, session, authorize, repository)
