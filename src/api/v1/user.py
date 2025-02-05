bdf267aaf12ebec446d98d075c0706d9c153848e
evtepo <andrey_laguta00@mail.ru>
2025-02-05 21:55:37 +1000

    add get handler for list of users

--- src/api/v1/user.py
+++ src/api/v1/user.py
@@ -1,5 +1,5 @@
 from async_fastapi_jwt_auth.auth_jwt import AuthJWT
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from starlette import status
 
 from schemas.user import UserCreate, UserDelete, UserInfo, UserUpdate
@@ -8,34 +8,56 @@ from services.user import (
     create_user_profile,
     delete_user_profile,
     get_user_info,
+    get_list_users,
     update_user_profile,
 )
 from utils.dependency import db_dependency, repository_dependency
+from utils.error_handling import error_response
 
 
 router = APIRouter(prefix="/api/v1/user", tags=["User"])
 
 
 @router.get(
-    "/",
+    "/{email}",
     summary="Get user info.",
     status_code=status.HTTP_200_OK,
     response_model=UserInfo,
 )
 async def get_user(
+    email: str,
     session: db_dependency,
     repository: repository_dependency,
     authorize: AuthJWT = Depends(),
-    email: str | None = None,
 ):
     current_user = await check_current_user(session, authorize, repository)
-    if current_user.is_stuff and email:
+    if current_user.is_stuff and email and current_user.email != email:
         user_info = await get_user_info(session, repository, email)
 
         return user_info
 
     return current_user
 
+
+@router.get(
+    "/",
+    summary="Get list of users.",
+    status_code=status.HTTP_200_OK,
+    response_model=dict[str, dict[str, int] | list[UserInfo | None]]
+)
+async def get_users(
+    session: db_dependency,
+    repository: repository_dependency,
+    authorize: AuthJWT = Depends(),
+    page: int = Query(ge=1, default=1),
+    size: int = Query(ge=10, le=50, default=10),
+):
+    current_user = await check_current_user(session, authorize, repository)
+    if not current_user.is_stuff:
+        return error_response("Access Denied", status.HTTP_403_FORBIDDEN)
+
+    return await get_list_users(page, size, session, repository)
+    
 
 @router.post(
     "/",
