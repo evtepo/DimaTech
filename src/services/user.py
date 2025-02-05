93febdcedeb27ead1ec243a04342a7f0af5a3cd5
evtepo <andrey_laguta00@mail.ru>
2025-02-05 21:56:25 +1000

    add logic for list of users

--- src/services/user.py
+++ src/services/user.py
@@ -12,6 +12,29 @@ async def get_user_info(session: AsyncSession, repository: BaseRepository, email
     return await repository.get_single(User, session, **filters)
 
 
+async def get_list_users(
+    page: int,
+    size: int,
+    session: AsyncSession,
+    repository: BaseRepository,
+) -> dict[str, dict[str, int] | list[User | None]]:
+    prev_page = page - 1 if page - 1 > 0 else None
+    page = (page - 1) * size
+
+    data = await repository.get(User, session, size, page)
+
+    next_page = page + 1 if data and len(data) == size else None
+    result = {
+        "links": {
+            "prev": prev_page,
+            "next": next_page,
+        },
+        "data": data
+    }
+
+    return result
+
+
 async def create_user_profile(data: UserCreate, session: AsyncSession, repository: BaseRepository) -> User:
     user = await repository.create(data, User, session)
     if not user:
