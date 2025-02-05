75d3b12698a55945af97549ef818c117dffe22ae
evtepo <andrey_laguta00@mail.ru>
2025-02-05 19:40:47 +1000

    pydantic schemas for payment

---
+++ src/schemas/payment.py
@@ -0,0 +1,17 @@
+from uuid import UUID
+
+from pydantic import BaseModel
+
+
+class PaymentMixin(BaseModel):
+    transaction_id: UUID
+    amount: float
+    user_id: UUID
+    account_id: UUID
+
+
+class PaymentInfo(PaymentMixin):
+    id: UUID
+
+
+class PaymentCreate(PaymentMixin): ...
