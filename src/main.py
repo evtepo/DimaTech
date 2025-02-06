bcccf754735f6bb9d7e41738bc799629485b1131
evtepo <andrey_laguta00@mail.ru>
2025-02-06 23:00:32 +1000

    add routers

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
 
+from api.v1.account import router as account_router
 from api.v1.auth import router as auth_router
 from api.v1.user import router as user_router
+from api.v1.payment import router as payment_router
 from configs.settings import settings
 
 
 app = FastAPI()
 
+app.include_router(account_router)
 app.include_router(auth_router)
+app.include_router(payment_router)
 app.include_router(user_router)
 
 if __name__ == "__main__":
