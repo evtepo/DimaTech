a67682ea6d55673b7d6bbe4cab756f41876c9db7
evtepo <andrey_laguta00@mail.ru>
2025-02-06 23:04:19 +1000

    change fields

--- src/schemas/auth.py
+++ src/schemas/auth.py
@@ -6,3 +6,4 @@ class TokenSettings(BaseModel):
     authjwt_secret_key: str = settings.token_secret_key
     authjwt_algorithm: str = "HS256"
     authjwt_token_location: set = {"cookies"}
+    authjwt_cookie_csrf_protect: bool = False
