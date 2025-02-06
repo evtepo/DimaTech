f6cf27d26efccec8a642bda55b35f69d09e9bbf6
evtepo <andrey_laguta00@mail.ru>
2025-02-06 23:00:16 +1000

    fix current user check

--- src/api/v1/user.py
+++ src/api/v1/user.py
@@ -2,10 +2,10 @@ from async_fastapi_jwt_auth.auth_jwt import AuthJWT
 from fastapi import APIRouter, Depends, Query
 from starlette import status
 
-from schemas.user import UserCreate, UserDelete, UserInfo, UserUpdate
+from models.user import User
+from schemas.user import UserDelete, UserInfo, UserUpdate
 from services.auth import check_current_user
 from services.user import (
-    create_user_profile,
     delete_user_profile,
     get_user_info,
     get_list_users,
@@ -19,16 +19,16 @@ router = APIRouter(prefix="/api/v1/user", tags=["User"])
 
 
 @router.get(
-    "/{email}",
+    "/info",
     summary="Get user info.",
     status_code=status.HTTP_200_OK,
     response_model=UserInfo,
 )
 async def get_user(
-    email: str,
     session: db_dependency,
     repository: repository_dependency,
     authorize: AuthJWT = Depends(),
+    email: str | None = None,
 ):
     current_user = await check_current_user(session, authorize, repository)
     if current_user.is_stuff and email and current_user.email != email:
@@ -43,7 +43,7 @@ async def get_user(
     "/",
     summary="Get list of users.",
     status_code=status.HTTP_200_OK,
-    response_model=dict[str, dict[str, int] | list[UserInfo | None]]
+    response_model=dict[str, dict[str, int | None] | list[UserInfo | None]]
 )
 async def get_users(
     session: db_dependency,
@@ -53,45 +53,30 @@ async def get_users(
     size: int = Query(ge=10, le=50, default=10),
 ):
     current_user = await check_current_user(session, authorize, repository)
+    if not isinstance(current_user, User):
+        return current_user
+
     if not current_user.is_stuff:
         return error_response("Access Denied", status.HTTP_403_FORBIDDEN)
 
     return await get_list_users(page, size, session, repository)
-    
-
-@router.post(
-    "/",
-    summary="New user profile.",
-    status_code=status.HTTP_201_CREATED,
-    response_model=UserInfo,
-)
-async def create_profile(
-    user_data: UserCreate,
-    session: db_dependency,
-    repository: repository_dependency,
-    authorize: AuthJWT = Depends(),
-):
-    await check_current_user(session, authorize, repository)
-    
-    return await create_user_profile(user_data, session, repository)
 
 
 @router.put(
-    "/{email}",
+    "/",
     summary="Update user profile.",
     status_code=status.HTTP_200_OK,
     response_model=UserInfo,
 )
 async def update_profile(
-    email: str,
     user_data: UserUpdate,
     session: db_dependency,
     repository: repository_dependency,
-    authorize: AuthJWT,
+    authorize: AuthJWT = Depends(),
 ):
     await check_current_user(session, authorize, repository)
 
-    return await update_user_profile(user_data, email, session, repository)
+    return await update_user_profile(user_data, session, repository)
 
 
 @router.delete(
@@ -104,7 +89,7 @@ async def delete_profile(
     user: UserDelete,
     session: db_dependency,
     repository: repository_dependency,
-    authorize: AuthJWT,
+    authorize: AuthJWT = Depends(),
 ):
     await check_current_user(session, authorize, repository)
 
